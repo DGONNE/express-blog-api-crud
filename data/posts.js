f5112edf9d0463e521c87f70c4b2d96fe8feba0a
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-01-29 18:34:18 +0100

    posts.js ---> data

---
+++ data/posts.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    title: "Post 1",
+    content: "Contenuto del post 1",
+    tags: ["tech", "js"],
+  },
+  {
+    id: 2,
+    title: "Post 2",
+    content: "Contenuto del post 2",
+    tags: ["life", "health"],
+  },
+];
