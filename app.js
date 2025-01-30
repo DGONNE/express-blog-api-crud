4dbac94f03f7a58543db3610de3fb0ae8f0e620c
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-01-30 23:10:12 +0100

    aggiunta middleware per rotte non registrate e per gestione errori

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const port = 3002;
 
 app.use(express.json());
 
-app.use(postRoutes);
-
 app.use(bodyParser.json());
 app.use(postRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route non trovata" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Si è verificato un errore interno" });
+});
 
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
