1dab3a1e6df8363f32f82ca755e8896abc98a79c
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-01-30 23:03:30 +0100

    aggiunta rotta update alle rotte

--- routers/postRoutes.js
+++ routers/postRoutes.js
@@ -7,4 +7,6 @@ router.get("/posts", postController.index);
 router.get("/posts/:id", postController.show);
 router.delete("/posts/:id", postController.destroy);
 router.post("/posts", postController.store);
+router.put("/posts/:id", postController.update);
+
 module.exports = router;
